686e6672b7b785f8ec219495b5722e54f6cb199c
spencersoundy <112649118+spencersoundy@users.noreply.github.com>
2023-10-13 15:54:26 +0200

    full stack store
    created a home page, product page search page, 404 page, cart, seller page, about page, contact page, dashboard, login, search, sign up page using html css and js as my main back end  language
    
    i used node.js
    express.js for my server
    brcypt for my passwords
    firebase for my data base
    and aws to store images

---
+++ .history/public/js/nav_20231012150813.js
@@ -0,0 +1,69 @@
+// navbar
+
+const navbar = document.querySelector('.navbar');
+
+window.addEventListener('scroll', () => {
+    if(scrollY >= 180){
+        navbar.classList.add('bg');
+    } else{
+        navbar.classList.remove('bg');
+    }
+})
+
+const createNavbar = () => {
+    let navbar = document.querySelector('.navbar');
+
+    navbar.innerHTML += `
+    <ul class="links-container">
+    <li class="link-item"><a href="#" class="link active">home</a></li>
+        <li class="link-item"><a href="#" class="link">product</a></li>
+        <li class="link-item"><a href="#" class="link">about</a></li>
+        <li class="link-item"><a href="#" class="link">contact</a></li>
+    </ul>
+    <div class="user-interactions">
+        <div class="search-box">
+            <input type="text" class="search" placeholder="search item">
+            <button class="search-btn"><img src="../img/search.png" alt=""></button>
+        </div>
+        <div class="cart">
+            <img src="../img/cart.png" class="cart-icon" alt="">
+            <span class="cart-item-count">00</span>
+        </div>
+        <div class="user">
+            <img src="../img/user.png" class="user-icon" alt="">
+            <div class="user-icon-popup">
+                <p>login to your account</p>
+                <a>login</a>
+            </div>
+        </div>
+    </div>
+
+}
+
+createNavbar();
+
+// user icon popup
+
+let userIcon = document.querySelector('.user-icon');
+let userPopupIcon = document.querySelector('.user-icon-popup');
+
+userIcon.addEventListener('click', () => userPopupIcon.classList.toggle('active'))
+
+let text = userPopupIcon.querySelector('p');
+let actionBtn = userPopupIcon.querySelector('a');
+let user = JSON.parse(sessionStorage.user || null);
+
+if(user != null){ // user is logged in
+    text.innerHTML = `logged in as, ${user.name}`;
+    actionBtn.innerHTML = 'log out';
+    actionBtn.addEventListener('click', () => logout());
+} else{
+    text.innerHTML = 'login to your account';
+    actionBtn.innerHTML = 'login';
+    actionBtn.addEventListener('click', () => location.href = '/login');
+}
+
+const logout = () => {
+    sessionStorage.clear()
+    location.reload();
+}
---
+++ .history/server_20231012141714.js
@@ -0,0 +1,290 @@
+import express from "express";
+import bcrypt from "bcrypt";
+import { initializeApp } from "firebase/app";
+import { getFirestore, doc, collection, setDoc, getDoc, updateDoc, getDocs, query, where, deleteDoc } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDHf69lQ144fhun7L5mofvUi0CAycrVgc4",
+    authDomain: "ecom-website2-c02cf.firebaseapp.com",
+    projectId: "ecom-website2-c02cf",
+    storageBucket: "ecom-website2-c02cf.appspot.com",
+    messagingSenderId: "299819465312",
+    appId: "1:299819465312:web:baa904fd8da77d8e0d6105"
+  };
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+const db = getFirestore();
+
+// init server
+const app = express();
+
+// middlewares
+app.use(express.static("public"));
+app.use(express.json()) // enables form sharing
+
+//aws
+import aws from "aws-sdk";
+import "dotenv/config";
+
+//aws setup
+const region = "eu-central-1";
+const bucketName = "ecomm-websitefs";
+const accessKeyId = process.env.AWS_ACCESS_KEY;
+const secretAccessKey = process.env.AWS_SECRET_KEY;
+
+aws.config.update({
+    region,
+    accessKeyId,
+    secretAccessKey
+})
+
+//init s3
+const s3 = new aws.S3();
+
+// generate image url
+async function generateURL(){
+    let date = new Date();
+
+    const imageName = `${date.getTime()}.jpeg`;
+
+    const params = {
+        Bucket: bucketName,
+        Key: imageName,
+        Expires: 300, // 300 ms
+        ContentType: "image/jpeg"
+    }
+
+    const uploadURL = await s3.getSignedUrlPromise("putObject", params);
+    return uploadURL;
+}
+
+app.get('/s3url', (req, res) => {
+    generateURL().then(url => res.json(url));
+})
+
+// routes
+// home route
+app.get('/', (req, res) => {
+    res.sendFile("index.html", { root : "public" })
+})
+
+// signup
+app.get('/signup', (req, res) => {
+    res.sendFile("signup.html", { root : "public" })
+})
+
+app.post('/signup', (req, res) => {
+    const { name, email, password, number, tac } = req.body;
+
+    // form validations
+    if(name.length < 3){
+        res.json({ 'alert' : 'name must be 3 letters long'});
+    } else if(!email.length){
+        res.json({ 'alert' : 'enter your email'});
+    } else if(password.length < 8){
+        res.json({ 'alert' : 'password must be 8 letters long'});
+    } else if(!Number(number) || number.length < 10){
+        res.json({ 'alert' : 'invalid number, please enter valid one'});
+    } else if(!tac){
+        res.json({ 'alert' : 'you must agree to our terms and condition'});
+    } else{
+        // store the data in db
+        const users = collection(db, "users");
+
+        getDoc(doc(users, email)).then(user => {
+            if(user.exists()){
+                return res.json({ 'alert' : 'email already exists' })
+            } else{
+                // encrypt the password
+                bcrypt.genSalt(10, (err, salt) => {
+                    bcrypt.hash(password, salt, (err, hash) => {
+                        req.body.password = hash;
+                        req.body.seller = false;
+
+                        // set the doc
+                        setDoc(doc(users, email), req.body).then(data => {
+                            res.json({
+                                name: req.body.name,
+                                email: req.body.email,
+                                seller: req.body.seller,
+                            })
+                        })
+                    })
+                })
+            }
+        })
+    }
+})
+
+app.get('/login', (req, res) => {
+    res.sendFile("login.html", { root : "public" })
+})
+
+app.post('/login', (req, res) => {
+    let { email, password } = req.body;
+
+    if(!email.length || !password.length){
+        return res.json({ 'alert' : 'fill all the inputs' })
+    } 
+
+    const users = collection(db, "users");
+
+    getDoc(doc(users, email))
+    .then(user => {
+        if(!user.exists()){
+            return res.json({'alert': 'email does not exists'});
+        } else{
+            bcrypt.compare(password, user.data().password, (err, result) => {
+                if(result) {
+                    let data = user.data();
+                    return res.json({
+                        name: data.name,
+                        email: data.email,
+                        seller: data.seller
+                    })
+                } else{
+                    return res.json({ 'alert' : 'password is incorrect'})
+                }
+            })
+        }
+    })
+})
+
+// seller route
+app.get('/seller', (req, res) => {
+    res.sendFile('seller.html', { root : "public" })
+})
+
+app.post('/seller', (req, res) => {
+    let { name, address, about, number, email } = req.body;
+
+    if(!name.length || !address.length || !about.length || number.length < 10 || !Number(number)){
+        return res.json({ 'alert' : 'some information(s) is/are incorrect' });
+    } else{
+        // update the seller status
+        const sellers = collection(db, "sellers");
+        setDoc(doc(sellers, email), req.body)
+        .then(data => {
+            const users = collection(db, "users");
+            updateDoc(doc(users, email), {
+                seller: true
+            })
+            .then(data => {
+                res.json({ 'seller' : true })
+            })
+        })
+    }
+})
+
+// dashboard
+app.get('/dashboard', (req, res) => {
+    res.sendFile('dashboard.html', { root: "public" });
+})
+
+// add product
+app.get('/add-product', (req, res) => {
+    res.sendFile('add-product.html', { root: "public" });
+})
+
+app.get('/add-product/:id', (req, res) => {
+    res.sendFile('add-product.html', { root: "public" });
+})
+
+app.post('/add-product', (req, res) => {
+    let { name, shortDes, detail, price, image, tags, email, draft, id } = req.body;
+
+    if(!draft){
+        if(!name.length){
+            res.json({'alert' : 'should enter product name'});
+        } else if(!shortDes.length){
+            res.json({'alert' : 'short des must be 80 letters long'});
+        } else if(!price.length || !Number(price)){
+            res.json({'alert' : 'enter valid price'});
+        } else if(!detail.length){
+            res.json({'alert' : 'must enter the detail'});
+        } else if(!tags.length){
+            res.json({'alert' : 'enter tags'});
+        }
+    }
+
+    // add-product
+
+    let docName = id == undefined ? `${name.toLowerCase()}-${Math.floor(Math.random() * 50000)}` : id;
+
+    let products = collection(db, "products");
+    setDoc(doc(products, docName), req.body)
+    .then(data => {
+        res.json({'product': name})
+    })
+    .catch(err => {
+        res.json({'alert': 'some error occured.'})
+    })
+})
+
+app.post('/get-products', (req, res) => {
+    let { email, id, tag } = req.body
+    
+    let products = collection(db, "products");
+    let docRef;
+
+    if(id){
+        docRef = getDoc(doc(products, id));
+    } else if(tag){
+        docRef = getDocs(query(products, where("tags", "array-contains", tag)))
+    } else{
+        docRef = getDocs(query(products, where("email", "==", email)))
+    }
+
+    docRef.then(products => {
+        if(products.empty){
+            return res.json('no products');
+        }
+        let productArr = [];
+        
+        if(id){
+            return res.json(products.data());
+        } else{
+            products.forEach(item => {
+                let data = item.data();
+                data.id = item.id;
+                productArr.push(data);
+            })    
+        }
+
+        res.json(productArr);
+    })
+})
+
+app.post('/delete-product', (req, res) => {
+    let { id } = req.body;
+
+    deleteDoc(doc(collection(db, "products"), id))
+    .then(data => {
+        res.json('success');
+    }).catch(err => {
+        res.json('err');
+    })
+})
+
+app.get('/products/:id', (req, res) => {
+    res.sendFile("product.html", { root : "public" })
+})
+
+app.get('/search/:key', (req, res) => {
+    res.sendFile("search.html", { root : "public" })
+})
+
+// 404 route
+app.get('/404', (req, res) => {
+    res.sendFile("404.html", { root : "public" })
+})
+
+app.use((req, res) => {
+    res.redirect('/404')
+})
+
+app.listen(3000, () => {
+    console.log('listening on port 3000');
+})
